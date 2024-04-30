8f8ee5287e06b9a49c733050d47c983fbca97b9d
Ryan Sargeson <35943901+sargey18@users.noreply.github.com>
2024-04-30 22:31:30 +0700

    icon for the drag not showing up

--- notion-clone/src/Page/Page.tsx
+++ notion-clone/src/Page/Page.tsx
@@ -2,27 +2,36 @@
 import { useFocusedNodeIndex } from "./useFocusedNodeIndex"
 import { Cover } from "./Cover"
 import { Spacer } from "./Spacer"
-import { NodeTypeSwitcher } from "../Node/NodeTypeSwitcher"
 import { Title } from "./Title"
 import {nanoid} from "nanoid"
 import { useAppState } from "../state/AppStateContext"
-
-
+import { DndContext, DragOverlay, DragEndEvent } from "@dnd-kit/core"
+import { SortableContext, verticalListSortingStrategy } from "@dnd-kit/sortable"
+import { NodeContainer } from "../Node/NodeContainer"
 
 export const Page = () => {
-    const {title, nodes, addNode, setTitle} = useAppState()
+    const {title, nodes, recordNodes, addNode, setTitle} = useAppState()
  
     const [focusedNodeIndex, setFocusedNodeIndex] = useFocusedNodeIndex({nodes})
 
+    const handleDragEvent = (event: DragEndEvent) => {
+        const {active, over} = event;
+        if (over?.id && active.id !== over?.id) {
+            recordNodes(active.id as string, over.id as string)
+        }
 
+        
+    }
 
     return (
         <>
             <Cover/>
             <div>
                 <Title addNode={addNode} title={title} changePageTitle={setTitle}/>
+               <DndContext onDragEnd={handleDragEvent}>
+                <SortableContext items={nodes} strategy={verticalListSortingStrategy}>
                 {nodes.map((node, index) => (
-                    <NodeTypeSwitcher
+                    <NodeContainer
                     
                         key={node.id}
                         node={node}
@@ -32,6 +41,9 @@ export const Page = () => {
            
                     />
                 ))}
+                </SortableContext>
+                <DragOverlay />
+                </DndContext>
                 <Spacer
                 handleClick={() => {
                     addNode({type: "text", value: "", id: nanoid()}, nodes.length)
--- notion-clone/src/state/usePageState.ts
+++ notion-clone/src/state/usePageState.ts
@@ -1,5 +1,6 @@
 import { Page, NodeData, NodeType } from "../utils/types";
 import { useImmer } from "use-immer";
+import { arrayMove } from "@dnd-kit/sortable";
 
 export const usePageState = (initialState: Page) => {
     const [page, setPage] = useImmer<Page>(initialState);
@@ -61,6 +62,15 @@ export const usePageState = (initialState: Page) => {
         });
     };
 
+    const recordNodes = (id1: string, id2: string) => {
+        setPage((draft) => {
+            const index1 = draft.nodes.findIndex(node => node.id === id1);
+            const index2 = draft.nodes.findIndex(node => node.id === id2);
+
+            draft.nodes = arrayMove(draft.nodes, index1, index2);
+        })
+    }
+
     return {
         nodes: page.nodes,
         title: page.title,
@@ -71,6 +81,7 @@ export const usePageState = (initialState: Page) => {
         changeNodeType,
         setTitle,
         setCoverImage,
-        setNodes
+        setNodes,
+        recordNodes
     };
 };
